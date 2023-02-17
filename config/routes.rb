cfbfc7ae3647493a8830eef9c97cc7ada80c411f
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-02-17 15:23:56 +0000

    [Finish]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/homes/about" => "homes#about", as: "about"
-  resources :users, only:[:show, :edit]
+  resources :users, only:[:show, :edit, :update]
 end
