fccc1a93fd3e6c63d2122f97152392aece8c1474
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-02-17 16:32:56 +0000

    [Uodate]4-17

--- db/migrate/20230212112911_create_post_images.rb
+++ db/migrate/20230212112911_create_post_images.rb
@@ -7,6 +7,14 @@ class CreatePostImages < ActiveRecord::Migration[6.1]
       t.text :caption
       t.integer :user_id
       t.timestamps
+    
+    end
+      
+     create_tabul :post_comments do |f|
+      
+      t.text :comment
+      t.integer :user_id
+      t.integer :post_image_id
       
     end
     
